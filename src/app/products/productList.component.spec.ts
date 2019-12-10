f70818e712a8a2ddb642e32a574cc60ebd55b89e
Brett Cunningham <bwcunninghamii@gmail.com>
2019-12-10 11:50:32 -0500

    implemented RGR for show image text with pipe interpolation

--- src/app/products/productList.component.spec.ts
+++ src/app/products/productList.component.spec.ts
@@ -1,8 +1,43 @@
 import { ProductListComponent } from "./productsList.component";
+import { async, TestBed } from "@angular/core/testing";
 describe("Product List Component", () => {
-  it("should have show image property", () => {
-    const component = new ProductListComponent();
-    console.log("component:", component.showImage);
-    expect(component.showImage).toBeDefined();
+  let fixture;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProductListComponent]
+    }).compileComponents();
+    fixture = TestBed.createComponent(ProductListComponent);
+  }));
+
+  it("should create the Product List Component", () => {
+    const PLComponent = fixture.debugElement.componentInstance;
+
+    expect(PLComponent).toBeTruthy();
+  });
+
+  it("should intialize a showImage property with a value of false", () => {
+    const PLComponent = fixture.debugElement.componentInstance;
+
+    expect(PLComponent.showImage).toBeDefined();
+    expect(PLComponent.showImage).toEqual(false);
+  });
+
+  it("should render hide Image on showImage=false and show image on showImage=true", () => {
+    const PLComponent = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    const resultFalse = compiled.querySelector("button").textContent;
+    console.log("PLComponent:", PLComponent);
+    console.log("result:", resultFalse);
+
+    PLComponent.showImage = true;
+    console.log("PLComponent:", PLComponent);
+    fixture.detectChanges();
+    const resultTrue = compiled.querySelector("button").textContent;
+    console.log("result:", resultTrue);
+
+    expect(resultFalse).toEqual("Show Image");
+    expect(resultTrue).toEqual("Hide Image");
   });
 });
